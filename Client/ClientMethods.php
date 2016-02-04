0e7d286b53276c7588f16ad6d6a1fcd2ec88cc38
Manuel Raya <manuel@arrogance.es>
2016-02-04 15:55:40 +0100

    Implemented a few api methods.

--- Client/ClientMethods.php
+++ Client/ClientMethods.php
@@ -28,6 +28,81 @@ abstract class ClientMethods
     protected $connection;
 
     /**
+     * Set the return data type for several api functions.
+     *
+     * @param string $type Options: array, csv, xml, json.
+     *
+     * @return mixed
+     */
+    public function setReturnType($type)
+    {
+        return $this->connection->get('setReturnType', [ 'returnType' => $type ]);
+    }
+
+    /**
+     * Get a list of smtp tags.
+     *
+     * @param array $options
+     *
+     * @return object The response
+     */
+    public function getSmtpTags(array $options = [])
+    {
+        return $this->connection->get('getSmtpTags', $options);
+    }
+
+    /**
+     * Get sent messages processed by Mailrelay system.
+     *
+     * @param array $options
+     *
+     * @return object The response
+     */
+    public function getSends(array $options = [])
+    {
+        return $this->connection->get('getSends', $options);
+    }
+
+    /**
+     * Get bounced messages within a specified date.
+     *
+     * @param string $date Date in the following format: YYYY-MM-DD
+     * @param array  $options
+     *
+     * @return mixed
+     */
+    public function getDeliveryErrors($date, array $options = [])
+    {
+        return $this->connection->get('getDeliveryErrors', array_merge([ 'date' => $date ], $options));
+    }
+
+    /**
+     * Get the full day log for a specified date.
+     *
+     * @param string $date Date in the following format: YYYY-MM-DD
+     * @param array $options
+     *
+     * @return mixed
+     */
+    public function getDayLog($date, array $options = [])
+    {
+        return $this->connection->get('getDayLog', array_merge([ 'date' => $date ], $options));
+    }
+
+    /**
+     * Get the number of messages sent to the specified recipient.
+     *
+     * @param string $email Recipient email
+     * @param string $date Date in the following format: YYYY-MM-DD
+     *
+     * @return mixed
+     */
+    public function getMailRcptNumber($email, $date)
+    {
+        return $this->connection->get('getMailRcptNumber', [ 'email'  => $email, 'date' => $date ]);
+    }
+
+    /**
      * @param array $options
      *
      * @return object The response
